bd0291ee5a0be8ebf4284580d5259fc44ce03583
Kohlenbaron28 <110342046+Kohlenbaron28@users.noreply.github.com>
2023-02-13 14:46:06 +0300

    подключаю редакс+стор+чекбоксы и сортировка

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,24 +1,83 @@
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
+import { sortByPrice, sortByDuration, sortByOptimal } from '../../store/ticketsSlice';
+
 import classes from './Filter.module.scss';
 
 export default function Filter() {
+  const [price, setPrice] = useState(false);
+  const [duration, setDuration] = useState(false);
+  const [optimal, setOptimal] = useState(false);
+
+  const dispatch = useDispatch();
+
   return (
     <form className={classes.form}>
-      <div className={classes['form_radio_btn first']}>
-        <input className={classes['form_radio_btn__input']} id="radio-1" type="radio" name="radio" value="1" />
+      <div
+        className={classes['form_radio_btn first']}
+        onClick={() => {
+          dispatch(sortByPrice());
+          setPrice(true);
+          setDuration(false);
+          setOptimal(false);
+          // console.log(e.target.value);
+        }}
+      >
+        <input
+          className={classes['form_radio_btn__input']}
+          id="radio-1"
+          type="radio"
+          name="Price"
+          value="1"
+          checked={price}
+        />
         <label className={classes['form_radio_btn__label']} htmlFor="radio-1">
           Самый дешевый
         </label>
       </div>
 
-      <div className={classes['form_radio_btn second']}>
-        <input className={classes['form_radio_btn__input']} id="radio-2" type="radio" name="radio" value="2" />
+      <div
+        className={classes['form_radio_btn second']}
+        onClick={() => {
+          dispatch(sortByDuration());
+          setPrice(false);
+          setDuration(true);
+          setOptimal(false);
+          //console.log(e.target.value);
+        }}
+      >
+        <input
+          className={classes['form_radio_btn__input']}
+          id="radio-2"
+          type="radio"
+          name="Duration"
+          value="2"
+          checked={duration}
+        />
         <label className={classes['form_radio_btn__label']} htmlFor="radio-2">
           Самый быстрый
         </label>
       </div>
 
-      <div className={classes['form_radio_btn']}>
-        <input className={classes['form_radio_btn__input']} id="radio-3" type="radio" name="radio" value="3" />
+      <div
+        className={classes['form_radio_btn']}
+        onClick={() => {
+          dispatch(sortByOptimal());
+          setPrice(false);
+          setDuration(false);
+          setOptimal(true);
+          //console.log(e.target.value);
+        }}
+      >
+        <input
+          className={classes['form_radio_btn__input']}
+          id="radio-3"
+          type="radio"
+          name="Optimal"
+          value="3"
+          checked={optimal}
+        />
         <label className={classes['form_radio_btn__label']} htmlFor="radio-3">
           Оптимальный
         </label>
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,38 +1,136 @@
+import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+
+import { setValueFilterTicket, switchFilterAll } from '../../store/ticketsSlice';
+
 import classes from './SideBar.module.scss';
 
-export default function SideBar() {
+const SideBar = () => {
+  const [checkedAllTicket, setCheckedAllTicket] = useState(true);
+  const [checkedZero, setCheckedZero] = useState(true);
+  const [checkedOne, setCheckedOne] = useState(true);
+  const [checkedTwo, setCheckedTwo] = useState(true);
+  const [checkedThree, setCheckedThree] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (checkedZero && checkedOne && checkedTwo && checkedThree) {
+      setCheckedAllTicket(true);
+    } else {
+      setCheckedAllTicket(false);
+    }
+  }, [checkedZero, checkedOne, checkedTwo, checkedThree]);
+  useEffect(() => {
+    dispatch(switchFilterAll(checkedAllTicket));
+  }, [dispatch, checkedAllTicket]);
+  useEffect(() => {
+    dispatch(setValueFilterTicket({ isChecked: checkedZero, filterValue: 0 }));
+  }, [checkedZero, dispatch]);
+  useEffect(() => {
+    dispatch(setValueFilterTicket({ isChecked: checkedOne, filterValue: 1 }));
+  }, [checkedOne, dispatch]);
+  useEffect(() => {
+    dispatch(setValueFilterTicket({ isChecked: checkedTwo, filterValue: 2 }));
+  }, [checkedTwo, dispatch]);
+  useEffect(() => {
+    dispatch(setValueFilterTicket({ isChecked: checkedThree, filterValue: 3 }));
+  }, [checkedThree, dispatch]);
+
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case 'All':
+        setCheckedAllTicket(e.target.checked);
+        break;
+      case 'Zero':
+        setCheckedZero(e.target.checked);
+        break;
+      case 'One':
+        setCheckedOne(e.target.checked);
+        break;
+      case 'Two':
+        setCheckedTwo(e.target.checked);
+        break;
+      case 'Three':
+        setCheckedThree(e.target.checked);
+        break;
+      default:
+        setCheckedAllTicket(e.target.checked);
+        setCheckedZero(e.target.checked);
+        setCheckedOne(e.target.checked);
+        setCheckedTwo(e.target.checked);
+        setCheckedThree(e.target.checked);
+    }
+  };
+
   return (
     <div className={classes.sideBar}>
       <h3 className={classes['sideBar__title']}>Количество пересадок</h3>
-
       <form action="" className={classes.option}>
         <label htmlFor="1" className={classes['sideBar__label']}>
-          <input type="checkbox" className={classes['sideBar__input']} id="1" />
+          <input
+            onChange={handleChange}
+            name="All"
+            type="checkbox"
+            className={classes['sideBar__input']}
+            id="1"
+            checked={checkedAllTicket}
+          />
           <span className={classes['sideBar__chekBox']}></span>
           Все
         </label>
 
         <label className={classes['sideBar__label']} htmlFor="2">
-          <input type="checkbox" className={classes['sideBar__input']} id="2" />
+          <input
+            onChange={handleChange}
+            name="Zero"
+            type="checkbox"
+            className={classes['sideBar__input']}
+            id="2"
+            checked={checkedZero}
+          />
           <span className={classes['sideBar__chekBox']}></span>
           Без пересадок
         </label>
 
         <label className={classes['sideBar__label']} htmlFor="3">
-          <input type="checkbox" className={classes['sideBar__input']} id="3" />
+          <input
+            onChange={handleChange}
+            name="One"
+            type="checkbox"
+            className={classes['sideBar__input']}
+            id="3"
+            checked={checkedOne}
+          />
           <span className={classes['sideBar__chekBox']}></span>1 пересадка
         </label>
 
         <label className={classes['sideBar__label']} htmlFor="4">
-          <input type="checkbox" className={classes['sideBar__input']} id="4" />
+          <input
+            onChange={handleChange}
+            name="Two"
+            type="checkbox"
+            className={classes['sideBar__input']}
+            id="4"
+            checked={checkedTwo}
+          />
           <span className={classes['sideBar__chekBox']}></span>2 пересадки
         </label>
 
         <label className={classes['sideBar__label']} htmlFor="5">
-          <input type="checkbox" className={classes['sideBar__input']} id="5" />
+          <input
+            onChange={handleChange}
+            name="Three"
+            type="checkbox"
+            className={classes['sideBar__input']}
+            id="5"
+            checked={checkedThree}
+          />
           <span className={classes['sideBar__chekBox']}></span>3 пересадки
         </label>
       </form>
     </div>
   );
-}
+};
+
+export default SideBar;
